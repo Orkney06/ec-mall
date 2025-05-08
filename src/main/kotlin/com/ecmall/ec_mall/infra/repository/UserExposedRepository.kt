e50128ddedcf35a62718277ddf7593069d41c5de
Orkney06 <kome8728@gmail.com>
2025-05-09 00:50:40 +0900

    add ユーザー挿入クエリ

--- src/main/kotlin/com/ecmall/ec_mall/infra/repository/UserExposedRepository.kt
+++ src/main/kotlin/com/ecmall/ec_mall/infra/repository/UserExposedRepository.kt
@@ -3,6 +3,9 @@ package com.ecmall.ec_mall.infra.repository
 import com.ecmall.ec_mall.domain.user.User
 import com.ecmall.ec_mall.domain.user.UserRepository
 import com.ecmall.ec_mall.infra.rds.DatabaseConnector
+import com.ecmall.ec_mall.infra.rds.ecmall.UserTable
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -11,5 +14,12 @@ class UserExposedRepository(
 ): UserRepository{
     override fun insert(user: User) {
         databaseConnector.connect()
+        transaction {
+            UserTable.insert {
+                it[id] = user.userId
+                it[membershipId] = user.membershipId
+                it[accountStatus] = user.accountStatus.toString()
+            }
+        }
     }
 }
